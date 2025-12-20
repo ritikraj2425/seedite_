c2bc103c4ced2da12702a50d42367f44141b73dd
Ritik Raj <ritiknst2425@gmail.com>
2025-12-20 21:25:25 +0530

    deleted backend

--- backend/routes/mockTestRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMockTestById, submitMockTest } = require('../controllers/mockTestController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/:id', protect, getMockTestById);
-router.post('/:id/submit', protect, submitMockTest);
-
-module.exports = router;
